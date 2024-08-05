56cd1a1d9be5521f9f35ef4bb2b7e2f96b2e0d2b
MStackPro <naszymanasseh@gmail.com>
2024-08-05 15:18:09 +0100

    modified

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,45 +1,46 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { Sheet, SheetContent, SheetTrigger} from "./ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { MdMenu } from "react-icons/md";
 import Link from "next/link";
-import { useState } from "react";
 
 const links = [
-    {name: "home", path: "/"},
-    {name: "services", path: "#services"},
-    {name: "resume", path: "#resume"},
-    {name: "work", path: "#work"},
-    {name: "contact", path: "#contact"},
-]
+  { name: "home", path: "/" },
+  { name: "services", path: "#services" },
+  { name: "resume", path: "#resume" },
+  { name: "work", path: "#work" },
+  { name: "contact", path: "#contact" },
+];
 const MobileNav = () => {
-    const [isSheetOpen, setIsSheetOpen] = useState()
-    const handleClick = () => {
-        setIsSheetOpen(false)
-    }
-    const pathname = usePathname()
+  const pathname = usePathname();
   return (
-    <Sheet open={isSheetOpen}>
-        <SheetTrigger className="flex justify-center items-center">
-            <MdMenu className="text-[32px] text-accent"/>
-        </SheetTrigger>
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <MdMenu className="text-[32px] text-accent" />
+      </SheetTrigger>
 
-        <SheetContent className="flex flex-col">
-
-            {/* NAV */}
-            <nav className="flex flex-col justify-center items-center gap-6 mt-28">
-                {links.map((link, index) => {
-                    return (
-                        <Link onClick={handleClick} href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}>
-                            {link.name}
-                        </Link>
-                    )
-                })}
-            </nav>
-        </SheetContent>
+      <SheetContent className="flex flex-col">
+        {/* NAV */}
+        <nav className="flex flex-col justify-center items-center gap-6 mt-28">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent"
+                } text-xl capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
     </Sheet>
-  )
-}
+  );
+};
 
-export default MobileNav
+export default MobileNav;
