828da4b4a9742858954b557249a69f4578f9e968
MStackPro <naszymanasseh@gmail.com>
2024-11-12 00:00:16 +0100

    modified navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
             key={index}
             className={`${
               link.path === pathname && "text-accent border-b-2 border-accent"
-            } capitalize hover:text-accent transition-all`}
+            } capitalize underline-hover hover:text-accent transition-all`}
           >
             {link.name}
           </Link>
