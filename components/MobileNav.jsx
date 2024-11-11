e6b5d85ff805279d58a85473e697631e7a61dc2a
MStackPro <naszymanasseh@gmail.com>
2024-11-12 00:32:09 +0100

    modified

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,61 +1,14 @@
-// "use client";
-
-// import { usePathname } from "next/navigation";
-// import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
-// import { MdMenu } from "react-icons/md";
-// import Link from "next/link";
-
-// const links = [
-//   { name: "home", path: "/" },
-//   { name: "services", path: "#services" },
-//   { name: "resume", path: "#resume" },
-//   { name: "work", path: "#work" },
-//   { name: "contact", path: "#contact" },
-// ];
-// const MobileNav = () => {
-//   const pathname = usePathname();
-//   return (
-//     <Sheet>
-//       <SheetTrigger className="flex justify-center items-center">
-//         <MdMenu className="text-[32px] text-accent" />
-//       </SheetTrigger>
-
-//       <SheetContent className="flex flex-col">
-//         {/* NAV */}
-//         <nav className="flex flex-col justify-center items-center gap-6 mt-28">
-//           {links.map((link, index) => {
-//             return (
-//               <Link
-//                 href={link.path}
-//                 key={index}
-//                 className={`${
-//                   link.path === pathname &&
-//                   "text-accent border-b-2 border-accent"
-//                 } text-xl capitalize hover:text-accent transition-all`}
-//               >
-//                 {link.name}
-//               </Link>
-//             );
-//           })}
-//         </nav>
-//       </SheetContent>
-//     </Sheet>
-//   );
-// };
-
-// export default MobileNav;
-
 "use client"
 
 import React, { useState } from "react";
-import Box from "@mui/material/Box";
-import SwipeableDrawer from "@mui/material/SwipeableDrawer";
-import List from "@mui/material/List";
 import { CiMenuBurger } from "react-icons/ci";
 import { IoIosArrowRoundBack } from "react-icons/io";
-import { IconButton } from "@mui/material";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { Box } from "@mui/material";
+import { List } from "@mui/material";
+import { SwipeableDrawer } from "@mui/material";
+import { IconButton } from "@mui/material";
 
 const links = [
   { name: "home", path: "/" },
