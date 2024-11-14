533fcb91433b7d16db85b9f7fd6b84ce06152747
MStackPro <naszymanasseh@gmail.com>
2024-11-14 09:07:27 +0100

    modified stats

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -3,6 +3,7 @@
 import { motion } from "framer-motion";
 import CountUp from "react-countup";
 import { fadeIn } from "./motions/variants";
+import { useInView } from "react-intersection-observer";
 
 
 const stats = [
@@ -13,22 +14,25 @@ const stats = [
 ];
 
 const Stats = () => {
+  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.5 })
   return (
     <section className="pt-4 pb-12 xl:pt-0 xl:pb-0 mx-auto">
       <div className="container mx-auto">
         <div className="grid grid-cols-2 xl:grid-cols-4 gap-6 max-w-[80vw] mx-auto xl:max-w-none">
           {stats.map((item, index) => {
             return (
-              <motion.div variants={fadeIn("up", 0.1)}
+              <motion.div ref={ref} variants={fadeIn("up", 0.1)} 
               initial="hidden"
               whileInView={"show"}
               viewport={{once: false, amount: 0.7}} className="flex-1 flex gap-4 items-center justify-center" key={index}>
-                <CountUp
+                { inView ? (
+                  <CountUp
                   end={item.num}
                   duration={5}
-                  delay={2}
+                  delay={1}
                   className="text-3xl xl:text-4xl font-extrabold"
                 />
+                ) : ( 0 )}
                 <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-sm` }>{item.text}</p>
               </motion.div>
             );
